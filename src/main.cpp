0bdc4715f694b6ab4843efc2654dbf27c587986c
Comp01-sudo <70709374+Comp01-sudo@users.noreply.github.com>
2021-06-01 08:25:43 -0400

    hh

--- src/main.cpp
+++ src/main.cpp
@@ -4,37 +4,33 @@
 
 #include <wiringPi.h>
 #include <softPwm.h>
-#include <network.h>
 
 #define RANGE		100
 #define	NUM_LEDS	  9
 
 int ledMap [NUM_LEDS] = { 0, 1, 2, 3, 4, 5, 6, 7, 8 } ;
 
-int values [NUM_LEDS] = { 0, 25, 50, 75, 100, 75, 50, 25 } ;
+int values [NUM_LEDS] = { 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 100 } ;
 
 int main ()
 {
+  int i, j;
+  char buf [80];
 
+  wiringPiSetup ();
 
-    
-  int i, j ;
-  char buf [80] ;
-
-  wiringPiSetup ()  ;
-
-  for (i = 0 ; i < NUM_LEDS ; ++i)
+  for (i = 0; i < NUM_LEDS; i++)
   {
-    softPwmCreate (ledMap [i], 0, RANGE) ;
-    printf ("%3d, %3d, %3d\n", i, ledMap [i], values [i]) ;
+    softPwmCreate (ledMap[i], 0, RANGE);
+    printf ("%3d, %3d, %3d\n", i, ledMap [i], values [i]);
   }
 
-  fgets (buf, 80, stdin) ;
+  fgets (buf, 80, stdin);
 
 // Bring all up one by one:
 
   for (i = 0 ; i < NUM_LEDS ; ++i)
-    for (j = 0 ; j <= 100 ; ++j)
+    for (j = 0 ; j <= j; ++j)
     {
       softPwmWrite (ledMap [i], j) ;
       delay (10) ;
@@ -65,7 +61,7 @@ int main ()
       values [j] = values [j + 1] ;
     values [NUM_LEDS - 1] = i ;
   }
-}int ledMap [NUM_LEDS] = { 0, 1, 2, 3, 4, 5, 6, 7 } ;
+}int ledMap [NUM_LEDS] = { 0, 1, 2, 3, 4, 5, 6, 7, 8 } ;
 
 int values [NUM_LEDS] = { 0, 25, 50, 75, 100, 75, 50, 25 } ;
 
